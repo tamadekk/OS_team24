d7507d800a1540bd795b0a3a6e0c8df5da57ecb2
Kiort <slava.vishnj@gmail.com>
2023-06-12 09:58:14 +0300

    aliluja

--- zd4.py
+++ zd4.py
@@ -3,8 +3,8 @@ import queue
 
 summ = 0  #summa svih prostih brojeva
 
-minq = queue.Queue(3)    #red naj manih prostih brojava u granama
-maxq = queue.Queue(3)    #red naj vecih prostih brojava u granama
+minq = queue.Queue(5)    #red naj manih prostih brojava u granama
+maxq = queue.Queue(5)    #red naj vecih prostih brojava u granama
 
 def IsPrime(n):     #algoritm trazenja prostog broja
     d = 2
@@ -12,28 +12,34 @@ def IsPrime(n):     #algoritm trazenja prostog broja
         d += 1
     return d * d > n
 def prime_numbers(startnum,stopnum):    #funkcija istrazivanja naimanih,naj visih i sume prostih broja
-    print ('.')
+
+
     global summ         #summa svih prostih brojeva
     minflag = 1 #flag za uhvat najmanjeg broja (prvi broj koj njemo)
-    min = None  # naj mani prosti broj
-    max = None  # naj veci prosti broj
+    min_prime = None  # naj mani prosti broj
+    max_prime = None  # naj veci prosti broj
+
     if(startnum==1):    #if za odabir grane sa diopozonom od 1
         summ+=3         #dodanje u sumu 1 i 2
         startnum=3      #promjena startnog broja na 3
-        min=1           #minimalni broj
+        min_prime=1           #minimalni broj
         minflag =0;     #promjena flaga za minimalni broj
+
     i = startnum        #pocetni broj za cikl
+
     while i < stopnum:   #pocetni cikl trazje prostih broja u diopozoni grane
         if(IsPrime(i)):     #provjera proja i, jeli prost broj
+
             if (minflag):   #provjera ili ikad upisivalose minimalni broj u ovom redku
-                min = i
+                min_prime = i
                 minflag = 0
+
             summ += i
-            print(i)
-            max = i
-        i+=1
-    minq.put(min)   #zapis u stup najmanjeg broja u ovom redku
-    maxq.put(max)   #zapis u stup najviseg broja u redku
+            max_prime = i
+
+        i += 1
+    minq.put(min_prime)   #zapis u stup najmanjeg broja u ovom redku
+    maxq.put(max_prime)   #zapis u stup najviseg broja u redku
 
 
 
@@ -49,16 +55,17 @@ thread1.join()      #cekanje zavrsetka rada 1 grane
 thread2.join()      #cekanje zavrsetka rada 2 grane
 thread3.join()      #cekanje zavrsetka rada 3 grane
 
-min = 1200000  # naj mani prosti broj   #naj visij moguci minimalni broj za prijavljeni redak
-max = 0  # naj veci prosti broj         #naj visij mani maxsimalni broj za prijavljeni redak
+#------------------------------------------------blok sortiranja i traznje potrebnih elemenata
+min_prime = minq.get()
+max_prime = maxq.get()
 
-while not minq.empty():             #ciklus potraznje najmalog minimuma
-    if min>minq.get_nowait():
-        min=minq.get()
-while  maxq.full():                 #ciklus potraznje najviseg maksimuma
-    if max<maxq.get_nowait():
-        max=maxq.get()
+while not minq.empty():
+    min_prime = min(min_prime, minq.get())
+while not maxq.empty():
+    max_prime = max(max_prime, maxq.get())
+#-------------------------------------------------
 
-f = open("minmax.txt", 'w')     #odrada dokumenta minmax.txt
-f = str(min) + '- minimalni broj\n' + str(max) + "- maximalni broj"     #upis minimuma i maxsimuma u dokument
-print ("summa prostih brojeva = " + str(summ) + "\nminimalni broj = " + str(min) +"\nmaxsimalni broj = " + str(max)) #izvodenje svih rezultatov
+    with open("minmax.txt", 'w') as file:               #otvorenje dokumenata minmax.txt
+        file.write(f"Minimalni broj: {min_prime}\n")
+        file.write(f"Maksimalni broj: {max_prime}\n")
+print ("summa prostih brojeva = " + str(summ) + "\nminimalni broj = " + str(min_prime) +"\nmaxsimalni broj = " + str(max_prime)) #izvodenje svih rezultatov
