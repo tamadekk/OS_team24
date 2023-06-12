a6052518e6943cc33ecd0aa735863ef828b9c5f0
Kiort <slava.vishnj@gmail.com>
2023-06-12 04:44:29 +0300

    not test

--- zd4.py
+++ zd4.py
@@ -1,65 +1,53 @@
-import threading,  os, os.path
+import threading
+import queue
 
-
-
+summ = 0
+min = 1200000
+max = 0
+minq = queue.Queue()
+maxq = queue.Queue()
 def prime_numbers(startnum,stopnum):
-    if (os.path.isdir("minmax.txt")):
-        file = open("minmax.txt", 'a')
-        file = 120000 + '\n' + 0 + '\n' + 0
-        file.close()
-    file = open("minmax.txt", 'w+')
+    global summ
     if(startnum==1):
-        summ=3
-        min=1
+        summ+=3
         startnum=3
-    else :
-        summ=0
-        max=0
-        min=1230000
 
-
-
+    minflag = 1
     for i in range(startnum,stopnum):
         flag = 1
         for j in range(2,i-1):
             if(i/j == int):
                     flag=0
                     break
-        if (flag):
-            summ+=i
-            if(min<i):
-                min=i
-            max=i
-
-    minlin = file.readline()
-    if min>minlin :
-        min=minlin
-    maxlin = file.readline()
-    if max<maxlin :
-        max=maxlin
-
-    summlin=int(file.readline()) + summ
-
-    file =minlin + '\n' + maxlin + '\n' + summlin
+            if (flag):
+                summ+=i
+                if( minflag):
+                    min=i
+                    minflag = 0
+                max=i
 
 
-thread1 = threading.Thread(target=prime_numbers(), args=(1,410000))
-thread2 = threading.Thread(target=prime_numbers(), args=(410001,820001))
-thread3 = threading.Thread(target=prime_numbers(), args=(820002,1230000))
+    minq.put(min)
+    max.put(max)
+
+
+thread1 = threading.Thread(target = prime_numbers, args = (1,410000))
+thread2 = threading.Thread(target = prime_numbers, args = (410001,820001))
+thread3 = threading.Thread(target = prime_numbers, args = (820002,1230000))
 
 thread1.start()
 thread2.start()
 thread3.start()
 
-thread1.join()
-thread1.join()
-thread1.join()
 
-file = open("minmax.txt", 'w+')
-min = file.readline()
-max = file.readline()
-summ = file.readline()
+while not minq.empty():
+    if min>minq.get_nowait():
+        min=minq.get()
+while  maxq.full():
+    if max<maxq.get_nowait():
+        max=maxq.get()
+
+file = open("minmax.txt", 'w')
 file = min + '- minimalni broj\n' + max + "- maximalni broj"
+file.close()
 print ("summa prostih brojeva = " + summ + "\nminimalni broj = " + min +"\nmaxsimalni broj = " + max)
-
-
