6642c06d16eae15ab2de591ad6d7aad0228a0d03
Kiort <slava.vishnj@gmail.com>
2023-06-12 03:12:42 +0300

    nou testing

--- zd4.py
+++ zd4.py
@@ -1,8 +1,11 @@
-import threading, time, os
+import threading, time, os, os.path
 
 
 
 def prime_numbers(startnum,stopnum):
+    if (path.isdir("minmax.txt")):
+        file = open("minmax.txt", 'a')
+        file.close()
     file = open("minmax.txt", 'w+')
     if(startnum==1):
         summ=3
@@ -16,9 +19,8 @@ def prime_numbers(startnum,stopnum):
 
     for i in range(startnum,stopnum):
         flag = 1
-        for j in range(startnum,i-1):
-            if(i!=j):
-                if(i/j == int):
+        for j in range(2,i-1):
+            if(i/j == int):
                     flag=0
                     break
         if (flag):
@@ -27,15 +29,34 @@ def prime_numbers(startnum,stopnum):
                 min=i
             max=i
 
+    minlin = file.readline()
+    if min>minlin :
+        min=minlin
+    maxlin = file.readline()
+    if max<maxlin :
+        max=maxlin
 
+    summlin=file.readline() + summ
 
-
-
-
-
+    file =minlin + '\n' + maxlin + '\n' + summlin
 
 
 thread1 = threading.Thread(target=prime_numbers(1,410000), args=(20,))
 thread2 = threading.Thread(target=prime_numbers(410001,820001), args=(50,))
 thread3 = threading.Thread(target=prime_numbers(820002,1230000), args=(10,))
+
+thread1.start()
+thread2.start()
+thread3.start()
+
+thread1.join()
+thread1.join()
+thread1.join()
+
+file = open("minmax.txt", 'w+')
+min = file.readline()
+max = file.readline()
+summ = file.readline()
+file = min + '- minimalni broj\n' + max + "- maximalni broj"
+print ("summa prostih brojeva = " + summ + "\nminimalni broj = " + min +"\nmaxsimalni broj = " + max)
 
